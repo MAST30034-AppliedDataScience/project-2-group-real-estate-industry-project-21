0d57db6b422ae68f145d21b500b1bcb138e677d7
AnshanAT <anshan.at@gmail.com>
2024-09-28 15:03:57 +1000

    optimised domain scraping code and accounted for a few potential errors

--- scripts/scrape_domain_links.py
+++ scripts/scrape_domain_links.py
@@ -1,6 +1,5 @@
 """
-A very simple, naive, and basic web scraping script. This will not
-work completely and is only applicable to domain.com.
+A basic web scraping script to get the links of properties in Melbourne's metropolitian area
 
 Feel free to use this as a source of inspiration, it is by no means production code.
 
@@ -38,7 +37,6 @@ BASE_URL = "https://www.domain.com.au"
 
 # begin code
 url_links = []
-property_metadata = defaultdict(dict)
 
 for suburb in list_of_suburbs[1:]: 
     
@@ -94,70 +92,7 @@ for suburb in list_of_suburbs[1:]:
 # The number of properties scraped... 
 print(f"The number of properties scraped is {len(url_links)}\n")
 
-# for each url, scrape some metadata
-pbar = tqdm(url_links[0:])
-success_count, total_count = 0, 0
-for property_url in pbar:
-    bs_object = BeautifulSoup(urlopen(Request(property_url, headers={'User-Agent':"PostmanRuntime/7.6.0"})), "lxml")
-    total_count += 1    
-    
-    try: 
-        # looks for the header class to get property name
-        property_metadata[property_url]['name'] = bs_object \
-            .find("h1", {"class": "css-164r41r"}) \
-            .text
-
-        # looks for the div containing a summary title for cost
-        property_metadata[property_url]['cost_text'] = bs_object \
-            .find("div", {"data-testid": "listing-details__summary-title"}) \
-            .text
-
-        # get rooms and parking
-        rooms = bs_object \
-                .find("div", {"data-testid": "property-features"}) \
-                .findAll("span", {"data-testid": "property-features-text-container"})
-
-        # rooms
-        property_metadata[property_url]['rooms'] = [
-            re.findall(r'\d+\s[A-Za-z]+', feature.text)[0] for feature in rooms
-            if 'Bed' in feature.text or 'Bath' in feature.text
-        ]
-        # parking
-        property_metadata[property_url]['parking'] = [
-            re.findall(r'\S+\s[A-Za-z]+', feature.text)[0] for feature in rooms
-            if 'Parking' in feature.text
-        ]
-
-        # Property description
-        prop_desc = bs_object \
-            .find("div", {"data-testid": "listing-details__description"}) \
-            .findAll("p")
-        
-        property_metadata[property_url]['description'] = "\n".join([p.text for p in prop_desc])
-
-        property_metadata[property_url]['prop_type'] = bs_object \
-            .find("div", {"data-testid": "listing-summary-property-type"}) \
-            .text
-        
-        # Additional features info... 
-        add_features = bs_object \
-            .find("ul", {"class": "css-4ewd2m"}) \
-            .findAll("li", {"data-testid": "listing-details__additional-features-listing"})
-        
-        property_metadata[property_url]['additional_features'] = [feature.text for feature in add_features]
-
-        success_count += 1
-        
-    except AttributeError:
-        print(f"Issue with {property_url}")
-
-    pbar.set_description(f"{(success_count/total_count * 100):.0f}% successful")
-
-# output to example json in data/raw/
-with open('./data/landing/domain_data.json', 'w') as f:
-    dump(property_metadata, f)
-    
-    
-
-
-
+# Save the links to a file 
+with open('./data/landing/property_urls.txt', 'w') as file:
+    for url in url_links:
+        file.write(url + '\n')
